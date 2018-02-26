0b3cd6600b3c8dad6584b604dd27bd2f4c21b69e
tzuri-orb <Tzuri.Kresh@orbotech.com>
2018-02-26 15:24:09 +0200

    change button

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+import {Meteor} from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
@@ -17,6 +18,7 @@ Template.hello.helpers({
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    // instance.counter.set(instance.counter.get() + 1);
+    Meteor.call('update')
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+  Meteor.methods({
+    update() {
+      require('child_process').execSync('/home/mct/fromhost');
+      console.log('did the deed');
+    }
+  });
   // code to run on server at startup
 });
